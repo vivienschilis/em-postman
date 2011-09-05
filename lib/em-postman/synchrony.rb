72ec52b195fc7431012bd2084df6e3a6e026e442
Vivien Schilis <vivien.schilis@gmail.com>
2011-09-05 02:32:06 +0100

    synchrony uses next_tick instead of add_timer

--- lib/em-postman/synchrony.rb
+++ lib/em-postman/synchrony.rb
@@ -2,12 +2,14 @@ module EventMachine
   class Postman
     def listen_messages(redis = new_redis_client)
       channel, message = redis.brpop(inbox_name, @timeout)
-      
+
       handle_rpop(channel, message) if channel && message
 
-      EM::Synchrony::add_timer(0) {
-        listen_messages(redis)
-      }
+      EM.next_tick do
+        Fiber.new { 
+          listen_messages(redis)
+        }.resume
+      end
     end
   end
 end
