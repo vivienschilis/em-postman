fdf15b727de02dc3c26470d1b0c1a0d1085d2362
Vivien Schilis <vivien.schilis@gmail.com>
2011-09-05 02:15:44 +0100

    Adding synchrony code

---
+++ lib/em-postman/synchrony.rb
@@ -0,0 +1,12 @@
+module EventMachine
+  class Postman
+    def listen_messages(redis = new_redis_client)
+      channel, message = redis.brpop(inbox_name, @timeout)
+      handle_rpop(channel, message)
+
+      EM::Synchrony::add_timer(0) {
+        listen_messages(redis)
+      }
+    end
+  end
+end
